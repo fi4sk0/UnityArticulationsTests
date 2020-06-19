fe59909203633f8b39d82255fb95f266088b5557
Timo Maul <hello@timomaul.de>
2020-06-19 09:51:50 +0200

    Initial commit

---
+++ Assets/ArticulationTest/ArticulationInfo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArticulationInfo : MonoBehaviour
+{
+    public ArticulationBody ab;
+    public Text text;
+    public Text debugButtonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ab != null && debugButtonText != null) {
+            debugButtonText.text = $"Info {ab.name}";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ab.jointType == ArticulationJointType.RevoluteJoint && ab.transform.parent != null) {
+            text.text = $"Actual {(ab.jointPosition[0] / Mathf.PI * 180f).ToString("F0")}";
+        } else {
+            text.text = $"Actual n/a";
+        }
+    }
+
+    public void SetAngle(float angle) {
+        var xDrive = ab.xDrive;
+        xDrive.target = angle;
+        ab.xDrive = xDrive;
+    }
+
+    [ContextMenu("Test")]
+    public void Test() {
+        ab.jointPosition = new ArticulationReducedSpace(Mathf.PI * 0.5f, 0, 0);
+    }
+
+
+    public void DebugInfo()
+    {
+        List<float> positions = new List<float>();
+        List<float> velocities = new List<float>();
+        
+        ab.GetJointPositions(positions);
+        ab.GetJointVelocities(velocities);
+
+        foreach (float p in positions)
+        {
+            Debug.Log($"position {p}");
+        }
+
+        foreach (float v in velocities)
+        {
+            Debug.Log($"velocity {v}");
+        }
+    }
+}
---
+++ Assets/ArticulationTest/SwapHierarchy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapHierarchy : MonoBehaviour
+{
+    public ArticulationBody parent;
+    public ArticulationBody child;
+
+    public Vector3 childAnchorPosition;
+    public Quaternion childAnchorRotation;
+    public Vector3 parentAnchorPosition;
+    public Quaternion parentAnchorRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentAnchorPosition = child.parentAnchorPosition;
+        parentAnchorRotation = child.parentAnchorRotation;
+        childAnchorPosition = child.anchorPosition;
+        childAnchorRotation = child.anchorRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (child.transform.parent == null)
+        {
+            (parent, child) = (child, parent);
+        }
+    }
+
+    public void Swap()
+    {
+        var formerChild = child;
+        var formerParent = parent;
+
+        child.transform.SetParent(parent.transform.parent, true);
+        parent.transform.SetParent(child.transform, true);
+
+        formerParent.jointType = formerChild.jointType;
+        formerParent.xDrive = formerChild.xDrive;
+
+        formerParent.anchorPosition = childAnchorPosition;
+        formerParent.anchorRotation = childAnchorRotation;
+        formerParent.parentAnchorPosition = parentAnchorPosition;
+        formerParent.parentAnchorRotation = parentAnchorRotation;
+
+    }
+}
